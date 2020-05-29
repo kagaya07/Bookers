d17a44efd448d32709db72c43cf8235b19c56e8c
vagrant <vagrant@localhost.localdomain>
2020-05-29 17:12:45 +0000

    [Add]4-8.3

--- db/migrate/20200528103448_create_books.rb
+++ db/migrate/20200528103448_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :title
-      t.text :body
+      t.string :title, presence:true
+      t.text :body, presence:true
 
       t.timestamps
     end
